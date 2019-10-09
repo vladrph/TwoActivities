66b8bd666c1f8fa263aa8bd365710a9d14421dfc
Vladimir <vrpetithomme2599@eagle.fgcu.edu>
2019-10-08 22:09:17 -0400

    2.2 Modify th AndroidManifest.xml added . 2.4 Add an intent to the main Activity fixed android:parentActivityName 3.3 Add a TextView to SecondActivity for the message

--- app/src/main/java/android/fundamentals/twoactivities/MainActivity.java
+++ app/src/main/java/android/fundamentals/twoactivities/MainActivity.java
@@ -6,21 +6,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String Extra_mssage = ".com.example.android.twoactivites.extra.MESSAGE";
+    private EditText mMessageEditText;
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mMessageEditText = findViewById(R.id.editText_main);
     }
 
     public void launchSecondActivity(View view) {
 
         Log.d(LOG_TAG, "Button Clicked!");
         Intent intent = new Intent(this, SecondActivity.class);
+        String message = mMessageEditText.getText().toString();
+        intent.putExtra(Extra_mssage, message);
         startActivity(intent);
     }
 }
