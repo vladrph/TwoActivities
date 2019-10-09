c3fc7182173a7bbcc9c2c1add422799adf762087
Vladimir <vrpetithomme2599@eagle.fgcu.edu>
2019-10-09 13:12:41 -0400

    2.2 Modify th AndroidManifest.xml added . 2.4 Add an intent to the main Activity fixed android:parentActivityName 3.3 Add a TextView to SecondActivity for the message 4.1 Add an EditText and a Button to the SecondActivity Layout 4.3 AddTextView elements to display results

--- app/src/main/java/android/fundamentals/twoactivities/SecondActivity.java
+++ app/src/main/java/android/fundamentals/twoactivities/SecondActivity.java
@@ -5,15 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import javax.xml.transform.Result;
+
 public class SecondActivity extends AppCompatActivity {
+    public static final String Extra_Reply =
+                                "com.example.android.twoactivities.extra.Reply";
+    private EditText mReply;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        mReply = findViewById(R.id.editText_second);
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.text_message);
@@ -22,5 +30,10 @@ public class SecondActivity extends AppCompatActivity {
     }
 
     public void returnReply(View view) {
+        String reply = mReply.getText().toString();
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(Extra_Reply,reply);
+        setResult(RESULT_OK, replyIntent);
+        finish();
     }
 }
